a44062ba0594e7ca4502ed28310b1734d196d4b4
buonzz <buonzz@gmail.com>
2014-12-07 02:43:46 +0800

    basic package

---
+++ src/Buonzz/Evorg/EvorgServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Buonzz\Evorg;
+
+use Illuminate\Support\ServiceProvider;
+
+class EvorgServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('buonzz/evorg');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
