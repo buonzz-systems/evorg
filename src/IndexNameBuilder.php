cc1014bf075203b7bbf95c2c0ef4b5fe61857251
buonzz <buonzz@gmail.com>
2016-11-06 07:10:21 +0800

    add dash

--- src/IndexNameBuilder.php
+++ src/IndexNameBuilder.php
@@ -1,12 +1,11 @@
 <?php namespace Buonzz\Evorg;
 
-
 class IndexNameBuilder{
 
 	public function build($eventName){
 
 		$indexname = 'evorg-events-'. \Config::get('evorg.app_id') 
-			. '-' . $eventName . date("Y") . "." . date("m");
+			. '-' . $eventName . "-". date("Y") . "." . date("m");
 
 		return $indexname;
 	}
