8bd58762d87cc226172e822ffb5d6df22b656d13
buonzz <buonzz@gmail.com>
2016-11-05 23:15:07 +0800

    migrate to L5.3

--- src/Buonzz/Evorg/EvorgFacade.php
+++
@@ -1,7 +0,0 @@
-<?php namespace Buonzz\Evorg;
-
-use Illuminate\Support\Facades\Facade;
-
-class EvorgFacade extends Facade{   
-   protected static function getFacadeAccessor(){ return 'evorg';}
-}
--- src/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(	
-	'app_id' => '',		
-	'app_name' => '',
-	'logging' => true,
-	'number_of_shards' => 3,
-	'number_of_replicas' => 2,
-	'hosts' => array('localhost:9200')	
-);
